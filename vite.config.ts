b335ec024e061830ce9aa03c91c778bca36d95b3
Gaurav kumar jha <igauravkumarjha@gmail.com>
2025-10-07 12:24:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: process.env.VITE_BASE_PATH || "/Real-Time-Captioning-Solution",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
